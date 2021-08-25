efec229e18a71d6c236868a82fe17cc89e00de28
F9oOol11 <faisal-2052@hotmail.com>
2021-08-25 13:09:59 +0300

    shuffle names array

--- app/static/js/draw.js
+++ app/static/js/draw.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
       .then(data => { 
         for(var x=0; x < data.length; x++){
             nameArray.push(data[x].full_name);
-            console.log(nameArray[x]);
         }
       })
+      nameArray= shuffle(nameArray);
 })
 
 $("#pick").click(function() {
@@ -146,3 +146,20 @@ $("#close").click(function() {
 
 }).call(this);
 
+function shuffle(array) {
+  var currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
