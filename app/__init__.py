c46af785565a4ad904ede3db7aeaef89e9b2fc21
F9oOol11 <faisal-2052@hotmail.com>
2021-08-30 10:41:16 +0300

    production

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 if ENV == 'dev':
     app.debug = True
     app.config['SECRET_KEY'] = 'c9aafc85052059bcf8c42237bf291ae9295a294dcc707384b47d94b340bae26a'
@@ -25,7 +25,8 @@ else:
     app.config['SECRET_KEY'] = 'c9aafc85052059bcf8c42237bf291ae9295a294dcc707384b47d94b340bae26a'
 #   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yilboziuuucmpj:f7c22115880b874cff646a60f4d81940159242a60c4e760defb193b123f24e93@ec2-50-17-255-120.compute-1.amazonaws.com:5432/dcjg9oq80q0h3v'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qkpbisijwohqrm:89247105548928c4ad710b14e92636b98207043586eec7363ff46ef179e1bbb3@ec2-34-194-14-176.compute-1.amazonaws.com:5432/d3k031h0jklro0'
-    app.config['MAIL_SERVER'] = 'smtp.mailgun.org'
+    # app.config['MAIL_SERVER'] = 'smtp.mailgun.org'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     
