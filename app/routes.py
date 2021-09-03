53ee9bf0366f8a2dd41edc2aebbc0ad18a4ab0ab
F9oOol11 <faisal-2052@hotmail.com>
2021-09-03 20:30:07 +0300

    changing download table.xlsxl directory

--- app/routes.py
+++ app/routes.py
@@ -300,7 +300,7 @@ def export():
                 data = Record.query.all()
                 data_list =[to_dict(item) for item in data]
                 df = pd.DataFrame(data_list)
-                filename = 'static/excel/table.xlsx'
+                filename = url_for('static', filename='excel/table.xlsx')
                 writer = pd.ExcelWriter(filename)
                 df.to_excel(writer, sheet_name='records')
                 writer.save()
